2abcbeb1f4f1ca4056d68ccc4a8d25cf619c9ee6
ts <tian970623@163.com>
2022-12-02 22:53:46 +0800

    新增Test

---
+++ src/main/java/com/ts/hrms/Test.java
@@ -0,0 +1,16 @@
+package com.ts.hrms;
+/**
+ * @BelongsProject: hrms-master
+ * @BelongsPackage: com.ts.hrms
+ * @Author: ts
+ * @CreateTime: 2022-12-02  22:51
+ * @Description: TODO
+ * @Version: 1.0
+*/
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
+
