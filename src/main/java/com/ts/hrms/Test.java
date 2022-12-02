4b47fb7a1a71acf451ca42d491ec8669f0202364
ts <tian970623@163.com>
2022-12-02 23:44:11 +0800

    2022年12月2日23:43:58 ssh-git

--- src/main/java/com/ts/hrms/Test.java
+++ src/main/java/com/ts/hrms/Test.java
@@ -12,6 +12,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("7851136");
         System.out.println("123456");
+        System.out.println("ssh");
     }
 }
 
